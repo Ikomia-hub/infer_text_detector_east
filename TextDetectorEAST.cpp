55d9e09e5870cb40422c9a2e99ab03441d357a95
Ludo <ludovic.barusseau@ikomia.com>
2023-03-14 17:46:44 +0100

    Fix: cast error on Windows

--- TextDetectorEAST.cpp
+++ TextDetectorEAST.cpp
@@ -25,9 +25,9 @@ void CTextDetectorEAST::init()
     for (size_t i=0; i<m_classNames.size(); ++i)
     {
         CColor color = {
-            (int)((double)std::rand() * factor),
-            (int)((double)std::rand() * factor),
-            (int)((double)std::rand() * factor)
+            (uchar)((double)std::rand() * factor),
+            (uchar)((double)std::rand() * factor),
+            (uchar)((double)std::rand() * factor)
         };
         m_classColors.push_back(color);
     }
